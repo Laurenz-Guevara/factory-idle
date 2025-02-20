73afb9631c7eeb94ef1677ec49a01d0a59195464
Laurenz Guevara <laurenzguevara@outlook.com>
2025-02-20 18:35:03 +0000

    feat: add taildwind css

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from 'react-dom/client';
 
+export default function App() {
+  return (
+    <div>
+      <p>Hello World</p>
+    </div>
+  );
+}
+
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<App />);
