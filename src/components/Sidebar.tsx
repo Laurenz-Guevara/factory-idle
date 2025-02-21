d4eef3f1d4bc8d728a8eab9d395847827274fb83
Laurenz Guevara <laurenzguevara@outlook.com>
2025-02-21 02:24:40 +0000

    refactor: cleaned up text styling

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,23 +10,27 @@ export default function SideBar() {
         <div className="flex-1 px-4 space-y-4">
           <div>
             <p className="px-4 text-sm font-medium text-gray-500">Extraction</p>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Pickaxe />Mining</a>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><TreePine />Woodcutting</a>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Droplet />Oil</a>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Octagon />Gas</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Pickaxe />Mining</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><TreePine />Woodcutting</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Droplet />Oil</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Octagon />Gas</a>
           </div>
           <div>
             <p className="px-4 text-sm font-medium text-gray-500">Production</p>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Flame />Smelting</a>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Factory />Refining</a>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Bomb />Fracking</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Flame />Smelting</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Factory />Refining</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Bomb />Fracking</a>
           </div>
           <div>
             <p className="px-4 text-sm font-medium text-gray-500">Other</p>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Truck />Transporting</a>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Fan />Power</a>
-            <a className="flex items-center gap-x-2 px-4 py-2 text-sm font-medium rounded-lg text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"><Gem />Selling</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Truck />Transporting</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Fan />Power</a>
+            <a className="flex items-center gap-x-2 px-4 py-2 hover:bg-gray-700 cursor-pointer"><Gem />Selling</a>
           </div>
+        </div>
+        <div className="grid grid-rows-2 px-4 py-2 space-y-2">
+          <a>Report a bug</a>
+          <a>Privacy Policy</a>
         </div>
         <div className="bg-slate-700 mb-6">
           <p className="text-center text-gray-400">V1.0.0</p>
